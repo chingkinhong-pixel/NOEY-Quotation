bcf026a6ddd17f33475b5ec6b399b4d1d0643de1
chingkinhong-pixel <chingkinhong@gmail.com>
2026-08-19 22:04:04 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -799,7 +799,7 @@ export default function App() {
             snap_door_brand: dbCab.snap_door_brand || '', snap_door_color: dbCab.snap_door_color || '',
             door_unit_adjustment: dbCab.door_unit_adjustment || '', door_material_remark: dbCab.door_material_remark || '',
             snap_door_surface_finish: dbCab.snap_door_surface_finish || '', upgrades: cabUpgrades,
-            // 【新增】：回填 countertop 数据，提供绝对的安全兜底防报错
+           // 【新增】：回填 countertop 数据，提供绝对的安全兜底防报错
             countertop: dbCab.countertop || {
               enabled: false,
               material: '',
